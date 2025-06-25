f0bcb78d558df4458c9ccf4caebc76636593481d
Ruarai Tobin <ruarai.tobin@unimelb.edu.au>
2025-06-25 13:38:46 +1000

    Add SIMD titre-normal logpdf calculation (danger)

--- src/distributions/titre_arraynormal.jl
+++ src/distributions/titre_arraynormal.jl
@@ -45,6 +45,37 @@ function apply_logpdf(x, μ, σ, min, max)
     return l_sum
 end
 
+function apply_logpdf_simd(x, μ, σ, min_val, max_val)
+    l_sum = 0.0
+    inv_sigma_sqrt2 = 1.0 / (σ * sqrt(2.0))
+    @turbo for i in eachindex(x)
+        arg1 = (x[i] - μ[i]) * inv_sigma_sqrt2
+        arg2 = (x[i] + 1.0 - μ[i]) * inv_sigma_sqrt2
+        arg_min = (min_val + 1.0 - μ[i]) * inv_sigma_sqrt2
+        arg_max = (max_val - μ[i]) * inv_sigma_sqrt2
+
+        # Branch 1: x <= min
+        # result1 = normlogcdf(μ, σ, min + 1.0)
+        logcdf_min = log(0.5 * (1.0 + erf(arg_min)))
+
+        # Branch 2: x >= max
+        # result2 = normlogccdf(μ, σ, max)
+        logccdf_max = log(0.5 * (1.0 - erf(arg_max)))
+
+        # Branch 3: min < x < max
+        # result3 = logsubexp(normlogcdf(μ, σ, x + 1.0), normlogcdf(μ, σ, x))
+        logcdf_x1 = log(0.5 * (1.0 + erf(arg2)))
+        logcdf_x = log(0.5 * (1.0 + erf(arg1)))
+        logsubexp_val = logcdf_x1 + log1p(-exp(logcdf_x - logcdf_x1))
+
+        mask1 = x[i] <= min_val
+        mask2 = x[i] >= max_val
+
+        l_sum += ifelse(mask1, logcdf_min, ifelse(mask2, logccdf_max, logsubexp_val))
+    end
+    return ifelse(isnan(l_sum), -Inf, l_sum)
+end
+
 # A more direct translation of Kucharski
 # note the treatment of max
 function titre_logpdf_component_uncorrected(x::S, μ::T, σ::T, min::T, max::T) where {T <: Real, S <: Real}
@@ -68,7 +99,7 @@ end
 
 function Distributions.logpdf(d::TitreArrayNormal{T}, x::AbstractVector{S})::T where {T <: Real, S <: Real}
     if d.corrected
-        return apply_logpdf(x, d.μ, d.σ, d.minimum, d.maximum)
+        return apply_logpdf_simd(x, d.μ, d.σ, d.minimum, d.maximum)
     else
         return apply_logpdf_uncorrected(x, d.μ, d.σ, d.minimum, d.maximum)
     end
